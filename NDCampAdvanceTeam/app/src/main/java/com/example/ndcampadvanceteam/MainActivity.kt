1979d5973274813ec3479b40fbcdf3ff220f0cc8
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-13 18:10:05 +0900

    create&config viewbinding mainactivity

--- NDCampAdvanceTeam/app/src/main/java/com/example/ndcampadvanceteam/MainActivity.kt
+++ NDCampAdvanceTeam/app/src/main/java/com/example/ndcampadvanceteam/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ndcampadvanceteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.viewpager2.widget.ViewPager2
+import com.example.ndcampadvanceteam.databinding.MainActivityBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private val binding: MainActivityBinding by lazy { MainActivityBinding.inflate(layoutInflater) }
+    private val mainTabLayout: TabLayout by lazy { binding.mainTabLayout }
+    private val mainViewPager: ViewPager2 by lazy { binding.mainViewPager }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
---
+++ NDCampAdvanceTeam/app/src/main/java/com/example/ndcampadvanceteam/MainTodoFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ndcampadvanceteam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ndcampadvanceteam.databinding.MainTodoFragmentBinding
+
+class MainTodoFragment : Fragment() {
+    private val binding: MainTodoFragmentBinding by lazy { MainTodoFragmentBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    companion object {
+    }
+}
