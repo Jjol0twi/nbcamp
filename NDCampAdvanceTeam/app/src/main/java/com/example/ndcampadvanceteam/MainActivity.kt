f3a9a17b415676292826773815bf521aa71b2c92
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-14 00:22:45 +0900

    add floatingbutton binding&config bookmark fragment

--- NDCampAdvanceTeam/app/src/main/java/com/example/ndcampadvanceteam/MainActivity.kt
+++ NDCampAdvanceTeam/app/src/main/java/com/example/ndcampadvanceteam/MainActivity.kt
@@ -5,16 +5,17 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.ndcampadvanceteam.databinding.MainActivityBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     private val binding: MainActivityBinding by lazy { MainActivityBinding.inflate(layoutInflater) }
     private val mainTabLayout: TabLayout by lazy { binding.mainTabLayout }
     private val mainViewPager: ViewPager2 by lazy { binding.mainViewPager }
+    private val mainFloatingButton: FloatingActionButton by lazy { binding.mainFloatingButton }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
 }
