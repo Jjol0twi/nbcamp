380bd4f01670530ca215f5a056e19a8739a2a3d7
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-20 17:07:40 +0900

    add naivgationview click event&framgent change

--- KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/MainActivity.kt
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kakaobankfirsthalfassignments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import com.example.kakaobankfirsthalfassignments.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+        navigationView.setOnItemSelectedListener { it ->    // navigationView menu click event
+            when (it.itemId) {
+                R.id.my_storage -> {    // home menu
+                    displayFragment(MyStorageFragment())
+                }
+
+                R.id.search_results -> {    // my storage click
+                    displayFragment(SearchResultsFragment())
+                }
+            }
+            true
+        }
+        navigationView.selectedItemId = R.id.search_results // first screnn clicked
+    }
+
+    private fun displayFragment(fragment: Fragment) {   // fragment change
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)    // get fragment
+            .commit()
     }
 }
