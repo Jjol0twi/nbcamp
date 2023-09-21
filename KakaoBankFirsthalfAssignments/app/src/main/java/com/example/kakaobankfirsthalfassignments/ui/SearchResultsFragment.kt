e9422fd2324e4f2d217dd259917bd9260d93c710
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-22 03:50:14 +0900

    get data from restapi

--- KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/ui/SearchResultsFragment.kt
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/ui/SearchResultsFragment.kt
@@ -38,6 +38,48 @@ class SearchResultsFragment : Fragment() {
     private fun initView() = with(binding) {
         resultsGridView.adapter = listAdapter
         resultsGridView.layoutManager = GridLayoutManager(context, 2)
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //                kakaoSearchService.requestImage(query).enqueue(object : Callback<KakaoImageSearch> {
+//                    override fun onResponse(
+//                        call: Call<KakaoImageSearch>,
+//                        response: Response<KakaoImageSearch>
+//                    ) {
+//                        if (response.isSuccessful) {
+//                            Log.d("test", "response success")
+//                        } else {
+//                            Log.d("error", "response failed")
+//                        }
+//                    }
+//
+//                    override fun onFailure(call: Call<KakaoImageSearch>, t: Throwable) {
+//                        Log.d("error", "call api fail: " + t.message)
+//                    }
+//                })
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return getResultData(query)
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun getResultData(query: String?): Boolean {
+        if (query.isNullOrBlank()) return true
+        GlobalScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                val result =
+                    KakaoRepository().getImageData(query = query, sortType = "recency")
+                withContext(Dispatchers.Main) {
+                    listAdapter.refreshData(result)
+                }
+            }.onFailure {
+                Log.d("error", "response failed")
+            }
+        }
+        return false
     }
     override fun onDestroyView() {
         super.onDestroyView()
