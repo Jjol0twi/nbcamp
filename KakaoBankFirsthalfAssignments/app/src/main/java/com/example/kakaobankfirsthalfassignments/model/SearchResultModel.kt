1d1062d3e6aeb36397b44e3d40d1e2d67f1d78a9
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-22 04:05:05 +0900

    add searchresult custom data class

---
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/model/SearchResultModel.kt
@@ -0,0 +1,8 @@
+package com.example.kakaobankfirsthalfassignments.model
+
+data class SearchResultModel(
+    val previewImg :String,
+    val title : String,
+    val postTime : String,
+    val isStorage : Boolean = false,
+)
