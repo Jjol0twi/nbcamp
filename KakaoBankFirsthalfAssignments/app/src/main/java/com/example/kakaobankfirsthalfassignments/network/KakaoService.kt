ddd37aa500521c2dc39f87fad5b611d297ff1206
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-22 03:53:48 +0900

    add video search with coroutine

--- KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/network/KakaoService.kt
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/network/KakaoService.kt
@@ -1,6 +1,8 @@
 package com.example.kakaobankfirsthalfassignments.network
 
 import com.example.kakaobankfirsthalfassignments.BuildConfig
+import com.example.kakaobankfirsthalfassignments.model.KakaoImageModel
+import com.example.kakaobankfirsthalfassignments.model.KakaoVideoModel
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
@@ -16,6 +18,15 @@ interface KakaoService {
         @Query("page") page: Int = 1,
         @Query("size") size: Int = 80,
     ): KakaoImageModel
+
+    @GET("v2/search/vclip")
+    suspend fun requestVideo(
+        @Query("query") text: String,
+        @Query("sort") sortType: String = "accuracy",
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 80,
+    ): KakaoVideoModel
+
 //    suspend fun requestImage(
 //        @Header("Authorization") apiKey: String = BuildConfig.kakao_key_restapi,
 //        @Query("query") text: String,
