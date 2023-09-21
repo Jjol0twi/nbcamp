d83ede6fd4b2a19234a5c8a6a995ea669c1665e2
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-22 03:48:25 +0900

    add search image with coroutine

--- KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/network/KakaoService.kt
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/network/KakaoService.kt
@@ -1,30 +1,33 @@
 package com.example.kakaobankfirsthalfassignments.network
 
 import com.example.kakaobankfirsthalfassignments.BuildConfig
-import com.example.kakaobankfirsthalfassignments.model.KakaoImageSearch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
-import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface NetworkService {
+interface KakaoService {
 
     @Headers("Authorization: ${BuildConfig.kakao_key_restapi}")
     @GET("v2/search/image")
-    suspend fun searchKakao(
+    suspend fun requestImage(
         @Query("query") text: String,
         @Query("sort") sortType: String = "accuracy",
         @Query("page") page: Int = 1,
         @Query("size") size: Int = 80,
-    ): KakaoImageSearch
-//    suspend fun searchKakao(
-//        @Header("Authorization") apiKey : String = BuildConfig.kakao_key_restapi,
-//        @Query("query") text : String,
-//        @Query("sort") sortType : String = "accuracy",
-//        @Query("page") page : Int = 1,
-//        @Query("size") size : Int = 80,
-//    ): KakaoImageSearch
+    ): KakaoImageModel
+//    suspend fun requestImage(
+//        @Header("Authorization") apiKey: String = BuildConfig.kakao_key_restapi,
+//        @Query("query") text: String,
+//        @Query("sort") sortType: String = "accuracy",
+//        @Query("page") page: Int = 1,
+//        @Query("size") size: Int = 80,
+//    ): KakaoImageModel
+//    fun requestImage(
+//        @Query("query") text: String,
+//        @Query("sort") sortType: String = "accuracy",
+//        @Query("page") page: Int = 1,
+//        @Query("size") size: Int = 80,
+//    ): Call<KakaoImageSearch>
 
 }
