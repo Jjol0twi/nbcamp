7e83d5d4cdfab6f1c5cf25215e1c2aee671c58dd
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-22 03:53:07 +0900

    get data from fragment&show data with item ui

--- KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/adpater/SearchResultsListAdapter.kt
+++ KakaoBankFirsthalfAssignments/app/src/main/java/com/example/kakaobankfirsthalfassignments/adpater/SearchResultsListAdapter.kt
@@ -1,23 +1,29 @@
 package com.example.kakaobankfirsthalfassignments.adpater
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.kakaobankfirsthalfassignments.databinding.ItemSerachResultsBinding
-import com.example.kakaobankfirsthalfassignments.model.KakaoApiSearchImage
+import com.bumptech.glide.Glide
+import com.example.kakaobankfirsthalfassignments.databinding.ItemSearchResultsBinding
+import com.example.kakaobankfirsthalfassignments.model.KakaoImageModel
 
 class SearchResultsListAdapter : RecyclerView.Adapter<SearchResultsListAdapter.ViewHolder>() {
-    class ViewHolder(private val binding: ItemSerachResultsBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-            fun bind() = with(binding){
 
-            }
     private var itemList: ArrayList<KakaoImageModel.Document> = arrayListOf()
+
+    fun refreshData(item: KakaoImageModel) {
+        itemList = item.documents
+        notifyDataSetChanged()
     }
 
     class ViewHolder(private val binding: ItemSearchResultsBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: KakaoImageModel.Document) = with(binding) {
+            Glide.with(binding.root)
+                .load(item.thumbnailUrl)
+                .into(previewImg)
+            resultTitle.text = item.siteName
+            resultDate.text = item.datetime
         }
     }
 
